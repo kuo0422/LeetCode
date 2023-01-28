fb491bf98ebda63f57558a110413cf55f50bbd03
kristen <m4466059@gmail.com>
2023-01-29 00:08:40 +0800

    1480

---
+++ LeetCode 75/LeetCode75/LeetCode75/Code/Running_Sum_of_1d_Array_1480.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode75.Code
+{
+    internal class Running_Sum_of_1d_Array_1480
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            var result = new int[nums.Length];
+            Array.Copy(nums, 0, result, 0, nums.Length);
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                result[i] += result[i - 1];
+            }
+
+            return result;
+        }
+
+        internal void PrintResult(int[] data, int[] result)
+        {
+            var printValue = String.Join(',', data);
+            var printResultValue = String.Join(',', result);
+
+            Console.WriteLine($"Input: nums = [{printValue}]");
+            Console.WriteLine($"Output: [{printResultValue}]");
+        }
+
+        internal int[] GetInitialData()
+        {
+            return new int[] { 1, 2, 3, 4 };
+        }
+    }
+}
---
+++ LeetCode 75/LeetCode75/LeetCode75/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode75.Code;
+
+Console.WriteLine("1480. Running_Sum_of_1d_Array");
+Console.WriteLine("-----------------------------");
+var Code1480 = new Running_Sum_of_1d_Array_1480();
+var data = Code1480.GetInitialData();
+var result = Code1480.RunningSum(data);
+Code1480.PrintResult(data, result);
+Console.WriteLine();
